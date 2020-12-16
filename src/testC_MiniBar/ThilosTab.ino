e01dda933e4fba9bdc891b59eef64cc555f86784
richi <ristein@posteo.de>
2020-12-16 18:44:35 +0100

    added Tab for everyone

---
+++ src/testC_MiniBar/ThilosTab.ino
@@ -0,0 +1,16 @@
+//Klassendefinitionen
+class Konto
+{
+  public:
+
+    char Benutzer[30];
+    float Kontostand;
+};
+
+class Getraenk
+{
+  public:
+
+    char Bezeichnung[30];
+    float Preis;
+};
--- src/testC_MiniBar/testC_MiniBar.ino
+++ src/testC_MiniBar/testC_MiniBar.ino
@@ -1,28 +1,12 @@
-//Klassendefinitionen
-class Konto
-{
-  public:
-
-    char Benutzer[30];
-    float Kontostand;
-};
-
-class Getraenk
-{
-  public:
-  
-    char Bezeichnung[30];
-    float Preis;
-};
+#include <LiquidCrystal>
 
 void setup() {
   Serial.begin(115200);
   Serial.println();
   Serial.println("MiniBar, 2020-12-14");
-  //add a comment
 }
 
 void loop() {
   delay(333);
-  Serial.println(millis());
+  Serial.println(String(millis()) +" "+String(test()));
 }
